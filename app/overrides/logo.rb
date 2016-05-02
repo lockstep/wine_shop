10d37f797763310690bae7a0a56578eb6f74a6cb
Paul Hiatt <hiattp@gmail.com>
2016-05-02 08:37:03 +0700

    improved styles

--- app/overrides/logo.rb
+++ app/overrides/logo.rb
@@ -1,8 +1,12 @@
-Deface::Override.new(:virtual_path => 'spree/shared/_header',
-  :name => 'logo',
-  :replace_contents => "#logo",
-  :text => "
-    <%= link_to root_path do %>
-    <%= image_tag 'logo.png', alt: 'GetWineOnline.com' %>
-    <% end %>
-  ")
+logo_markup = <<-ERB
+  <%= link_to root_path do %>
+  <%= image_tag 'logo.png', alt: 'GetWineOnline.com' %>
+  <% end %>
+ERB
+
+Deface::Override.new(
+  virtual_path: 'spree/shared/_header',
+  name: 'logo',
+  replace_contents: "#logo",
+  text: logo_markup
+)
